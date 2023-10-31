a8130aed4f0a6bee14b80cc7ba6e1fe30f54b19f
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-10-31 01:35:53 +0000

    FEATURE : Fix deployment

--- hitchskeleton/template.py
+++ hitchskeleton/template.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import os
 from sys import exit
 import shutil
+import stat
 
 
 @click.command()
-def website():
+@click.argument("name")
+def website(name):
     homedir = Path(os.path.expanduser("~"))
     cacheroot = homedir / ".cache"
     cachedir = cacheroot / "hitchskeleton"
@@ -19,7 +21,7 @@ def website():
         cacheroot.mkdir()
 
     if cachedir.exists():
-        cachedir.rmtree()
+        shutil.rmtree(cachedir)
     cachedir.mkdir()
 
     if newhitchdir.exists():
@@ -30,17 +32,21 @@ def website():
 
     git("clone", "https://github.com/hitchdev/hitchstory.git").in_dir(cachedir).run()
 
-    shutil.copytree(
-        srchitch / "hitch", newhitchdir
-    )
+    shutil.copytree(srchitch / "hitch", newhitchdir)
 
-    shutil.copyfile(srchitch / "run.sh", currentdir / "run.sh")
+    newrunsh = currentdir / "run.sh"
+    shutil.copyfile(srchitch / "run.sh", newrunsh)
+    newrunsh.chmod(newrunsh.stat().st_mode | stat.S_IEXEC)
 
     for existing_story in newhitchdir.joinpath("story").glob("*.story"):
         os.remove(existing_story)
 
-    os.remove(newhitchdir / "selectors" / "selectors.yml")
+    for existing_doc in newhitchdir.joinpath("docs").glob("*"):
+        os.remove(existing_doc)
 
-    
+    click.echo("Renaming to {}".format(name))
+    newhitchdir.joinpath("PROJECT_NAME").write_text(name)
+
+    os.remove(newhitchdir / "selectors" / "selectors.yml")
 
     click.echo("Set up. Run ./run.sh make")
